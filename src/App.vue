db1c82307d622265618348f4cdeb0e1cb378fa65
Nestor Junior <contato.nestor@gmail.com>
2021-09-11 21:15:07 -0300

    random result question

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     
   </section>
   <section class="details">
-    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum sed, iste, deleniti, aperiam cumque repellendus hic nobis maxime inventore magni earum! Dolorum fuga dolore perferendis voluptates molestias voluptate quia doloremque.</h2>
+    <h2 v-html="this.question"></h2>
     <input type="radio" name="options" value="true">
     <label for="true">True</label>
     
@@ -18,11 +18,31 @@
 export default {
   name: 'App',
 
+  //Function
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswers: undefined,
+    }
+  },
+
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswers);
+      return answers;
+    }   
+  }, 
+
+  //Lifehooks cicle
   created() {
     this.axios
     .get('https://opentdb.com/api.php?amount=10&category=10')
     .then((response) => {
-      console.log(response.data.results[0])
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswers = response.data.results[0].correct_answer;
     })
   },
 }
@@ -31,28 +51,27 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 
-  button {
-    display: block;
-    margin: 10px auto;
-    padding: 15px 25px;
-    cursor: pointer;
-    min-width: 120px;
-    color: #fff;
-    background-color: #1867c0;
-    border: #1867c0;
-    border-radius: 5px;
-    &:hover {
-      background-color: #145cad;
+    button {
+      display: block;
+      margin: 10px auto;
+      padding: 15px 25px;
+      cursor: pointer;
+      min-width: 120px;
+      color: #fff;
+      background-color: #1867c0;
+      border: #1867c0;
+      border-radius: 5px;
+      &:hover {
+        background-color: #145cad;
+      }
     }
   }
-}
-
 </style>
